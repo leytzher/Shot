dd0b20d602fb36f43c721cd5cf11fd543d8a5d5a
Leytzher <leytzher@gmail.com>
2020-07-30 17:07:48 +0400

    Added temperature interpolation

--- src/Trajectory.jl
+++ src/Trajectory.jl
@@ -1,7 +1,19 @@
 using Interpolations
-
-mutable struct Point{T}
-    MD::T
-    TVDSS::T
+"""
+    getTVDSS(md::Float64, mds::Array{Float64}, tvds::Array{Float64})
+Interpolates an array of MD, TVDSS and gives the tvdss at a desired depth"""
+function getTVDSS(md::Float64, mds::Array{Float64},tvds::Array{Float64} )
+    ipt = LinearInterpolation(mds, tvds)
+    ko = ipt(md)
+    return ko
 end
 
+"""
+    getTemperature(md::Float64, mds::Array{Float64}, temps::Array{Float64})
+Interpolates an array of MD, Temperature and gives the temperature at a desired depth
+"""
+function getTemperature(md::Float64, mds::Array{Float64}, temps::Array{Float64})
+    ipt = LinearInterpolation(mds, temps)
+    ko = ipt(md)
+    return ko
+end
